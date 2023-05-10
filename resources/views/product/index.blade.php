83ce65f120cce4b97a0bc3f0f346187e95d99b93
odairsp <odair.sp@hotmail.com>
2023-05-10 11:02:25 -0300

    Changes to be committed: 	deleted:    resources/views/product/delete.blade.php 	modified:   resources/views/product/index.blade.php

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -3,62 +3,65 @@
 
 @section('main')
 
-@if (session('msg'))
-<div class="bg-success text-center text-light fs-4 p-3 justify-itens-center ">
-    <p>{{ session('msg') }}</p>
-    <div class="text-start d-flex justify-content-center">
-        <p class="fs-6">Produto - {{session('product')->name}}<br> Descrição - {{session('product')->description}}</p>
-    </div>
-</div>
-@endif
-@if (session('alert'))
-<div class="bg-warning text-center text-dark fs-4 p-3 justify-itens-center ">
-    <p>{{ session('alert') }}</p>
-    <div class="text-start d-flex justify-content-center">
-        <p class="fs-6">Produto - {{session('product')->name}}<br> Descrição - {{session('product')->description}}</p>
-    </div>
-</div>
-@endif
-
-<div class="container text-center">
-    <h1 class="m-5">Produtos</h1>
-    <a class="btn btn-success btn-sm mb-3" href="{{route('products.create')}}">Criar novo produto</a>
-</div>
-
-
-<div class="container d-flex flex-wrap w-100 justify-content-center">
-    @if (count($products)==0)
-    
-    @else
-    @foreach ($products as $product)
-    <div class="card m-2" style="width: 18rem;">
-        <div class="card-body">
-            <h4 class="card-title">{{ $product->name }}</h4>
-            <p class="card-text">{{ $product->description }}</p>
+    @if (session('msg'))
+        <div class="bg-success text-center text-light fs-4 p-3 justify-itens-center ">
+            <p>{{ session('msg') }}</p>
+            <div class="text-start d-flex justify-content-center">
+                <p class="fs-6">Produto - {{ session('product')->name }}<br> Descrição -
+                    {{ session('product')->description }}</p>
+            </div>
         </div>
-        <div class="d-flex flex-row justify-content-center pb-2">
-
-            <form action="{{route('products.edit',$product->id)}}" method="GET">
-                @csrf
-                <button class="btn btn-sm btn-warning" type="submit">Editar</button>
-
-            </form>
-
-            <form action="{{route('products.destroy', $product->id)}}" method="POST">
-                @method('delete')
-                @csrf
-
-                <button class="btn btn-sm btn-danger ms-2" type="submit">Excluir</button>
-
-            </form>
-
+    @endif
+    @if (session('alert'))
+        <div class="bg-warning text-center text-dark fs-4 p-3 justify-itens-center ">
+            <p>{{ session('alert') }}</p>
+            <div class="text-start d-flex justify-content-center">
+                <p class="fs-6">Produto - {{ session('product')->name }}<br> Descrição -
+                    {{ session('product')->description }}</p>
+            </div>
         </div>
-
-    </div>
-    @endforeach
     @endif
 
-</div>
+    <div class="container text-center">
+        <h1 class="m-5">Produtos</h1>
+        <a class="btn btn-success btn-sm mb-3" href="{{ route('products.create') }}">Criar novo produto</a>
+    </div>
+
+
+    <div class="container d-flex flex-wrap w-100 justify-content-center">
+        @if (count($products) == 0)
+        @else
+            @foreach ($products as $product)
+                <div class="card m-2" style="width: 18rem;">
+                    <div class="card-body">
+                        <h4 class="card-title">{{ $product->name }}</h4>
+                        <p class="card-text">{{ $product->description }}</p>
+                    </div>
+                    <div class="d-flex flex-row justify-content-center pb-2">
+
+                        <form action="{{ route('products.edit', $product->id) }}" method="GET">
+                            @csrf
+                            <button class="btn btn-sm btn-warning" type="submit">Editar</button>
+
+                        </form>
+
+                        <form action="{{ route('products.destroy', $product->id) }}" method="POST">
+                            @method('delete')
+                            @csrf
+
+                            <button class="btn btn-sm btn-danger ms-2"
+                                onclick="if(confirm('Deseja realmente excluir?')){if(confirm('Deseja realmente excluir?')){}else{return false;}}else{return false;}"
+                                type="submit">Excluir</button>
+
+                        </form>
+
+                    </div>
+
+                </div>
+            @endforeach
+        @endif
+
+    </div>
 
 
 @endsection
